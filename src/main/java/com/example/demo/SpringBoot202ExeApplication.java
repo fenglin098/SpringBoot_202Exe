6de825bfa8d159f3cb6e820a861eed783a5e2952
feng.lin098 <feng.lin098@gmail.com>
2019-10-07 16:10:45 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot202ExeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot202ExeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot202ExeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Student {
+    private String name;
+    private int age;
+    private int height;
+    private long phone_number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public long getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(long phone_number) {
+        this.phone_number = phone_number;
+    }
+}
